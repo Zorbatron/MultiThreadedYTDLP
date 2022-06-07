920f9e106ee1fc0845eccdad554c356532cb7287
Gabriel Sakash <gabrielsakash@gmail.com>
2022-06-06 21:04:19 -0400

    Do not expect it to work 101% of the time.

---
+++ MulitTheadYT_DLP.py
@@ -0,0 +1,30 @@
+import multiprocessing as mp
+from yt_dlp import YoutubeDL
+
+def download_video(link, args):
+    with YoutubeDL(args) as ydl:
+        ydl.download(link)
+
+if __name__ == '__main__':
+    url = input("Input playlist URL\n")
+    workerAmount = int(input("Input amount of videos to download in parrallel\n"))
+
+    ytdl = YoutubeDL( {'extract_flat': True, 'quiet': True} )
+    data = ytdl.extract_info(url, download=False)
+
+    processes = []
+
+    pool = mp.Pool(workerAmount)
+    for block in data['entries']:
+        r = pool.apply_async(download_video, args=[block['url'], {
+            #'format': 'best[ext=mp4]'
+        }])
+        processes.append(r)
+    
+    for r in processes:
+        r.wait()
+
+    pool.close()
+    pool.join()
+
+    print("Done!")
